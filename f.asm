0688fb3bddd3a455eb57750b9e11022423100100
David Jones <drj@pobox.com>
2016-08-23 20:51:57 +0100

    Write out buffer using syscall 1

--- f.asm
+++ f.asm
@@ -1,7 +1,19 @@
 BITS 64
+
+sys_write equ 1
+
+SECTION .data
+arg db "For64", 10
+arglen equ $-arg
+
 section .text
 global _start
 _start:
+mov rdi, 1 ; stdout
+mov rsi, arg
+mov rdx, arglen
+mov rax, sys_write
+syscall
+mov rdi, 0
 mov rax, 60
-mov rdi, rax
 syscall
