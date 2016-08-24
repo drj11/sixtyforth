1721e11b6ae8c5979321d26ba62b12df0954d873
David Jones <drj@pobox.com>
2016-08-24 21:02:22 +0100

    Convert asm program to read then write

--- f.asm
+++ f.asm
@@ -1,19 +1,31 @@
 BITS 64
 
+sys_read equ 0
 sys_write equ 1
 
+SECTION .bss
+buf RESB 8192
+buflen equ $-buf
+
 SECTION .data
-arg db "For64", 10
-arglen equ $-arg
 
 section .text
 global _start
 _start:
+
+mov rdi, 0 ; stdin
+mov rsi, buf
+mov rdx, buflen
+mov rax, sys_read
+syscall
+
+; write out the same number of bytes as read
+mov rdx, rax
 mov rdi, 1 ; stdout
-mov rsi, arg
-mov rdx, arglen
+mov rsi, buf
 mov rax, sys_write
 syscall
+
 mov rdi, 0
 mov rax, 60
 syscall
