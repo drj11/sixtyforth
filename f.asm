8277d132449405d200e1ad7c36f1219ec1d3c753
David Jones <drj@pobox.com>
2016-08-27 18:05:21 +0100

    Convert assembly in a more FORTH style

--- f.asm
+++ f.asm
@@ -6,33 +6,58 @@ sys_write EQU 1
 SECTION .bss
         buf RESB 8192
         buflen EQU $-buf
+        stack RESB 100000
 
 SECTION .data
 
 SECTION .text
 GLOBAL _start
 _start:
+        mov r8, stack
 
 ; This program prints rax.
-        mov rax, 314592
 
-; Iteratively divide RAX by 10, until it becomes zero.
+; LIT 314592
+        mov rax, 314592
+        mov [r8], rax
+        add r8, 8
+
+; Iteratively divide TOS by 10, until it becomes zero.
 ; Pushing each remainder as we go,
 ; counting the number of pushes in RBX.
         mov rbx, 0
         mov rcx, 10     ; radix
 div10:
+        ; 10DIV (DIVIDEND -- QUOTIENT)
         mov rdx, 0
+        mov rax, [r8-8]
         idiv rcx
-        push rdx
         inc rbx
+        mov [r8-8], rax
+
+        ; ( -- RDX)
+        mov [r8], rdx
+        add r8, 8
+
+        ; SWAP (A B -- B A)
+        mov rbp, [r8-16]
+        mov rdx, [r8-8]
+        mov [r8-16], rdx
+        mov [r8-8], rbp
+
+        ; branch if TOS not 0
+        mov rax, [r8-8]
         test rax, rax
         jnz div10
 
 ; pop all the residues into buf
+        ; DROP (A -- )
+        sub r8, 8
+
         mov rdx, buf
 popit:
-        pop rax
+        sub r8, 8
+        mov rax, [r8]
         add rax, 48
         mov [rdx], al
         inc rdx
@@ -40,9 +65,9 @@ popit:
         jnz popit
 
 ; write out buf
-        mov byte [rdx], 10
-        sub rdx, buf    ; the buffer length
+        mov byte [rdx], 10      ; Add LF to buffer
         inc rdx
+        sub rdx, buf    ; the buffer length
         mov rdi, 1      ; stdout
         mov rsi, buf
         mov rax, sys_write
