0137579b4f3ce80ca20539ac2afdd894c5d53c50
David Jones <drj@pobox.com>
2016-08-28 17:56:13 +0100

    Implement more of DOT in Forth
    In particular, note that the very useful 0BRANCH word
    can now be used.

--- f.asm
+++ f.asm
@@ -24,17 +24,43 @@ ipl:    DQ stdexe
 
 LIT:
         DQ implit
+ZEROBRANCH:
+        DQ impzerobranch
 SWAP:
         DQ impswap
+DUP:
+        DQ impdup
+DROP:
+        DQ impdrop
+NEQ0:
+        DQ impneq0
 DOT:
+; Observation: It is easy to calculate the least significant digit,
+; by dividing by 10 and taking the remainder.
+; We proceed by pushing the digits onto the stack,
+; pushing the least significant first.
+; This creates a stack of digits of variable length;
+; we mark the beginning of the stack of digits with
+; a sentinel value, which is 99 (which can't possible be a digit).
         DQ stdexe
         DQ LIT
         DQ 99
         DQ SWAP
+l1:     DQ LIT
+        DQ 10
+        DQ DIVMOD       ; -- Q R
+        DQ SWAP         ; -- R Q
+        DQ DUP          ; -- R Q Q
+        DQ NEQ0         ; -- R Q B
+        DQ ZEROBRANCH   ; -- R Q
+        DQ -8
+        DQ DROP
         DQ restofDOT
         DQ NEXTWORD
 restofDOT:
         DQ imprestofdot
+DIVMOD:
+        DQ impdivmod
 EXIT:
         DQ impexit
 NEXTWORD:
@@ -75,6 +101,20 @@ implit:
         add r8, 8
         jmp next
 
+impzerobranch:
+        ; read the next word as a relative offset;
+        ; pop the TOS and test it;
+        ; if it is 0 then branch by adding the offset
+        ; to CODEPOINTER.
+        mov rbx, [r9]
+        add r9, 8
+        mov rax, [r8]
+        sub r8, 8
+        test rax, rax
+        jnz next
+        lea r9, [r9 + 8*rbx]
+        jmp next
+
 impswap:
         ; SWAP (A B -- B A)
         mov rbp, [r8-16]
@@ -82,27 +122,32 @@ impswap:
         mov [r8-16], rdx
         mov [r8-8], rbp
         jmp next
+impdup:
+        ; DUP (A -- A A)
+        mov rdx, [r8-8]
+        mov [r8], rdx
+        add r8, 8
+        jmp next
+impdrop:
+        ; DROP (A -- )
+        sub r8, 8
+        jmp next
+impneq0:
+        ; NEQ0 (A -- Bool)
+        ; Result is 0 (TRUE) if A != 0;
+        ; Result is -1 (FALSE) otherwise.
+        mov rax, [r8-8]
+        sub rax, 1      ; is-zero now in Carry flag
+        sbb rax, rax
+        mov [r8], rax
+        jmp next
 
 impexit:
         mov rdi, 0
         mov rax, 60
         syscall
 
-imprestofdot: ; ( N 99 -- )
-; Observation: It is easy to calculate the least significant digit,
-; by dividing by 10 and taking the remainder.
-; We proceed by pushing the digits onto the stack,
-; pushing the least significant first.
-; This creates a stack of digits of variable length;
-; we mark the beginning of the stack of digits with
-; a sentinel value, which is 99 (which can't possible be a digit).
-
-div10:
-        ; LIT 10
-        mov qword [r8], 10
-        add r8, 8
-
-        ; /MOD (dividend divisor -- quotient remainder)
+impdivmod:      ; /MOD (dividend divisor -- quotient remainder)
         ; > RCX
         sub r8, 8
         mov rcx, [r8]
@@ -112,7 +157,6 @@ div10:
 
         mov rdx, 0
         idiv rcx
-        inc rbx
 
         ; RAX >
         mov [r8], rax
@@ -120,23 +164,11 @@ div10:
         ; RDX >
         mov [r8], rdx
         add r8, 8
-        ; end /MOD
+        jmp next
 
-        ; SWAP (A B -- B A)
-        mov rbp, [r8-16]
-        mov rdx, [r8-8]
-        mov [r8-16], rdx
-        mov [r8-8], rbp
-
-        ; branch if TOS not 0
-        mov rax, [r8-8]
-        test rax, rax
-        jnz div10
+imprestofdot: ; ( 99 dN ... d1 -- )
 
 ; pop all the residues into buf
-        ; DROP (A -- )
-        sub r8, 8
-
         mov rdx, buf
 popit:
         sub r8, 8
